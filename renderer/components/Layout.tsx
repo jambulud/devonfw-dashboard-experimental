da7862f4004e85ac1e66dc341c8e81744c2817c0
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-12-04 08:37:00 +0100

    added material-ui

--- renderer/components/Layout.tsx
+++ renderer/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from './Header';
+import Head from 'next/head';
 
 const layoutStyle = {
   margin: 20,
@@ -6,8 +7,13 @@ const layoutStyle = {
   border: '1px solid #DDD'
 };
 
-const Layout = (props : {children: any}) => (
+const Layout = (props: { children: any }) => (
   <div style={layoutStyle}>
+    <Head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <title>React App</title>
+    </Head>
     <Header />
     {props.children}
   </div>
